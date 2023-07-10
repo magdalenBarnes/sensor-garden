edc92a8fe75566b73b6f028dc7b1a5684914df06
Cheyenne Simmons <97414403+Simmo120@users.noreply.github.com>
2023-07-10 16:07:09 -0500

    SAME

--- signin.php
+++ signin.php
@@ -8,7 +8,7 @@ $dbUser = 'plants';
 $dbPass = 'watermePLEASE';
 
 try {
-  $pdo = new PDO("mysql:host=$host;dbname=$dbname", $email, $password);
+  $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   die("Database connection failed: " . $e->getMessage());
